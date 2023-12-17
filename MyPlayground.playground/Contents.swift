a8e0df93e3ac1f44c86232731a2bedfddebdd7b2
yukenz <yuyun.purniawan@gmail.com>
2023-12-17 18:55:32 +0700

    Add Day 4-6

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,7 @@
 import UIKit
 import CoreFoundation
 
+
 //== Type Data | Simple Type
 var greeting = "Hello, playground"
 greeting = "GoodBye"
@@ -130,3 +131,135 @@ default:
 
 var passingGrade = 0...1
 print(fullName[passingGrade])
+
+
+//== Loop
+
+//For
+for n in 1...3{
+    print("Loop : \(n)")
+}
+
+for _ in 1...3{
+    
+}
+
+//while
+var counter = 0
+while counter < 20{
+    counter += 1
+}
+
+var number: Int = 10
+while number > 0 {
+    //10 8 6 4 2
+    number -= 2
+    // 8 6 4 2 0
+    
+    if number == 2{
+        continue
+    }
+    if number % 2 == 0 {
+        print("\(number) is an even number.")
+    }
+}
+
+// Labeled Loop
+rootLoop: for n in 0...5{
+    n
+    var j = 0
+    
+    while j < 5{
+        
+        if n == 3{
+            break rootLoop
+        }
+        
+        j += 1
+    }
+}
+
+
+//== Function
+
+// Normal - Default - Diff ext-int
+func printHelp(nama name:String = "anonymouse"){
+    
+    let messages = """
+    Hello \(name) is a help message
+    """
+    
+    print(messages)
+}
+
+printHelp(nama: "yuyun")
+printHelp()
+
+// Return Value
+func square(_ number: Int)->Int{
+    return number * number
+}
+
+print(square(100))
+
+// Spread / Variadic
+func variadicFunc(_ ballsNum:Int...){
+    for ballNum in ballsNum{
+        ballNum
+    }
+}
+variadicFunc(1,2,3)
+
+enum MyErr:Error{
+    case WEAKPASS
+}
+
+//Error Handling
+func checkPassword(_ pass:String) throws->Bool{
+    if(pass == "pass"){
+        throw MyErr.WEAKPASS
+    }
+    
+    return true
+}
+
+do{
+    try checkPassword("pass")
+}catch{
+    print("Password weak")
+}
+
+//INOUT params - Mutatuing
+
+func mutateThisToZero(_ num: inout Int){
+    num = 0
+}
+
+var mutateNum = 10
+mutateThisToZero(&mutateNum)
+mutateNum
+
+//== Functional
+
+let doDriving = {
+    (transport:String) -> String in //Unuse if not consume
+    print("I Driving \(transport)")
+    return transport
+}
+
+func runClosures(_ callable:(String)->String){
+    callable("Car")
+}
+
+runClosures(doDriving)
+
+//Trailing closure syntax
+func travel(_ destination: String, _ runnable: () -> Void) {
+    print("I'm getting ready go to \(destination)")
+    runnable()
+    print("I arrived!")
+}
+
+travel("Bali") {
+    print("Now Goooooo!")
+}
